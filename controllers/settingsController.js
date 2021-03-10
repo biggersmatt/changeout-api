87f1a23491a771a40a9cf04e384c555580ba4c06
biggersmatt <biggersmatt73@gmail.com>
2021-03-10 15:04:56 -0800

    Updates settingsController

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -2,12 +2,16 @@ const db = require('../models');
 
 const index = (req, res) => {
   console.log('Settings Index');
+  db.Settings.find({},(err, settings) => {
+    if(err) return console.log(err);
+    res.json({settings});
+  })
 }
 
 const create = (req, res) => {
+  console.log('Settings Create');
   db.Settings.create(req.body, (err, newSettings) => {
     if(err) console.log(err);
-    console.log(newSettings)
     res.json({newSettings});
   })
 }
