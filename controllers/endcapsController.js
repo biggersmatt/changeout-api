b6630569f9bfbb08f41bc0838bb43446e3949864
biggersmatt <biggersmatt73@gmail.com>
2021-04-05 10:11:00 -0700

    Clears logs on Endcap index

--- controllers/endcapsController.js
+++ controllers/endcapsController.js
@@ -1,14 +1,11 @@
 const db = require('../models');
 
 const index = (req, res) => {
-  console.log(req.session.currentUser)
   if (!req.session.currentUser){
     res.status(400);
-    console.log('endcap index not logged in hit')
     res.send('Not logged In')
     return;
   }
-  console.log('endcap index hit')
   db.Endcap.find({user: req.session.currentUser._id})
     .populate('flankA flankB').exec((err, allEndcaps) => {
     if(err) return console.log(err);
@@ -29,7 +26,6 @@ const show = (req, res) => {
 }
 
 const create = (req, res) => {
-  console.log('hitting create route')
   if (!req.session.currentUser){
     res.status(400);
     res.send('Not logged In')
